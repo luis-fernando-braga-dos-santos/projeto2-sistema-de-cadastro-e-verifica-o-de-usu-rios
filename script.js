b779f87f7abdc09b90df42b9c471b6d9839ae054
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2025-08-22 19:17:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//criando variaveis
+let nome = prompt("digite seu nome")
+let idade = prompt("digite sua idade")
+let senha = prompt("digite sua senha")
+
+if (idade >=13) {
+    alert("cadastro permitido");
+
+
+//criando login
+let nome2 = prompt("digite seu nome novamente")
+let senha2 = prompt("confirme sua senha")
+
+if (nome == nome2 && senha == senha2)
+    {alert(`
+=============seu cadastro===============
+login realizado com sucesso!bem vindo
+a techSchool
+
+nome do usuario:${nome2}
+idade do usuario:${idade}
+senha do usuario:${senha2}
+        
+=========================================
+        `);
+    }else{
+        alert("nome ou senha incorretos tente novamente");
+    }
+
+}else{
+    alert("nao e possivel se cadastrar idade minima 13 anos");}
